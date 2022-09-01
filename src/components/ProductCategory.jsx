f4744b7c46a453d83a108cbf96f4f786fefb6433
Tolga <06thy40@gmail.com>
2022-09-01 15:45:37 +0300

    updated

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductCategory = ({ categoryList, handleFiltered }) => {
+  const [categories, setCategories] = useState([]);
+  const getCategories = async () => {
+    const url = "https://powerful-savannah-74330.herokuapp.com/categories";
+    try {
+      const { data } = await axios(url);
+
+      return setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <div className="category container">
       <div className="category__btn">
-        {categoryList.map((category, index) => {
+        {categories?.map((category, index) => {
           return (
-            <button
-              className="category__btn-filter"
-              key={index}
-              onClick={() => handleFiltered(category)}
-            >
-              {category}
-            </button>
+            <div key={index}>
+              <img src={category.image} alt={category.name} />
+              <button
+                className="category__btn-filter"
+                key={index}
+                // onClick={() => handleFiltered(category)}
+              >
+                {category.name}
+              </button>
+            </div>
           );
         })}
       </div>
